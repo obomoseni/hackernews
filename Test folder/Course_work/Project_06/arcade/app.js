1fb7e4751ec8cd10d1a92b93662b7fececd44f8f
Olapeju Omoseni <obomoseni@gmail.com>
2022-08-10 08:50:50 -0400

    updated changes: start button working, snake moving

--- Test folder/Course_work/Project_06/arcade/app.js
+++ Test folder/Course_work/Project_06/arcade/app.js
@@ -1,5 +1,8 @@
 const board = document.querySelector('#board');
 const table = document.createElement('table');
+const start = document.getElementById('start');
+
+
 function makeBoard() {
 
     for (let x = 0; x < 15; x++) {
@@ -12,48 +15,85 @@ function makeBoard() {
     }
     board.appendChild(table);
 }
-makeBoard()
-
-// Define snake
-let snake = {
-    head: [10, 5],
-    body: [[10, 6], [10, 7], [10, 8]],
-    nextDirection: [1, 0]
-}
-let apple = [11, 8]
-
-let gameState = {
-    apple: apple,
-    snake: snake,
-}
 
 function drawSnake() {
-    table.children.item(snake.head[0]).children.item(snake.head[1]).setAttribute('class', 'snake');
-    for (let i = 0; i < snake.body.length; i++) {
-        table.children.item(snake.body[i][0]).children.item(snake.body[i][1]).setAttribute('class', 'snake');
+    table.children.item(gameState.snake.head[0]).children.item(gameState.snake.head[1]).setAttribute('class', 'snake');
+    for (let i = 0; i < gameState.snake.body.length; i++) {
+        table.children.item(gameState.snake.body[i][0]).children.item(gameState.snake.body[i][1]).setAttribute('class', 'snake');
     }
-} drawSnake()
+}
 
 function drawApple() {
-    table.children.item(apple[0]).children.item(apple[1]).setAttribute('id', 'apple');
+    table.children.item(gameState.apple[0]).children.item(gameState.apple[1]).setAttribute('id', 'apple');
 }
-drawApple()
+
+let gameState = {
+    snake: {},
+    apple: [],
+    speed: 0,
+    gameStart: false,
+    direction: '38'
+}
+function makeInitialGameState() {
+
+    makeBoard()
+
+    gameState.snake = {
+        head: [10, 5],
+        body: [[10, 6], [10, 7], [10, 8]],
+        nextDirection: [1, 0]
+    }
+    drawSnake()
+
+    gameState.apple = [11, 8]
+    drawApple()
+
+    gameState.speed = 1000
+}
+
+function snakeCollision() {
+    if (gameState.snake.head[0] == 15 || gameState.snake.head[0] == -1 || gameState.snake.head[1] == 15 || gameState.snake.head[1] == -1) {
+        gameState.gameStart = false;
+    }
+}
+function clearSnake(snakeSpeed) {
+    if (!gameState.gameStart) {
+        clearInterval(snakeSpeed)
+    }
+
+}
+
+function runningGameState() {
+    console.log("Running game state")
+    let snakeSpeed = setInterval(moveSnake, gameState.speed)
+    console.log("This is snake speed ", snakeSpeed)
+    setInterval(eatingApple, 10)
+    setInterval(snakeCollision, 10)
+    document.onkeydown = checkKey
+    setInterval(clearSnake.bind(null, snakeSpeed), 10)
+}
+makeInitialGameState();
+
+start.onclick = () => {
+    gameState.gameStart = true;
+    runningGameState()
+}
 
 function deleteApple() {
-    table.children.item(apple[0]).children.item(apple[1]).removeAttribute('id');
+    table.children.item(gameState.apple[0]).children.item(gameState.apple[1]).removeAttribute('id');
 }
 
 let score = 0
 
 function eatingApple() {
-    for (let i = 0; i < snake.body.length; i++) {
-        if (snake.body[i][0] === apple[0] && snake.body[i][1] === apple[1]) {
+    for (let i = 0; i < gameState.snake.body.length; i++) {
+        if (gameState.snake.body[i][0] === gameState.apple[0] && gameState.snake.body[i][1] === gameState.apple[1]) {
             min = Math.ceil(0);
             max = Math.floor(14);
             console.log(apple);
             deleteApple();
-            apple[0] = Math.floor(Math.random() * 15);
-            apple[1] = Math.floor(Math.random() * 15);
+            gameState.apple[0] = Math.floor(Math.random() * 15);
+            gameState.apple[1] = Math.floor(Math.random() * 15);
             console.log(apple);
             drawApple();
             score++
@@ -63,115 +103,114 @@ function eatingApple() {
     }
 }
 
-let previousMovement = '38'
-document.onkeydown = checkKey
+let direction = '38'
 function checkKey(e) {
     e = e || window.event;
     console.log(e.keyCode)
     let movement = e.keyCode;
-    if (movement == '38' && previousMovement != '40') {
+    if (movement == '38' && gameState.direction != '40') {
         moveSnake(e.keyCode)
-    } else if (movement == '40' && previousMovement != '38') {
+    } else if (movement == '40' && gameState.direction != '38') {
         moveSnake(e.keyCode)
-    } else if (movement == '37' && previousMovement != '39') {
+    } else if (movement == '37' && gameState.direction != '39') {
         moveSnake(e.keyCode)
-    } else if (movement == '39' && previousMovement != '37') {
+    } else if (movement == '39' && gameState.direction != '37') {
         moveSnake(e.keyCode);
-    } previousMovement = e.keyCode;
+    } gameState.direction = e.keyCode;
 }
 
-function moveSnake(direction) {
+function moveSnake() {
 
-    if (direction == '38') {
+    if (gameState.direction == '38') {
         deleteSnake()
-        snake.head[0] = snake.head[0] - 1
-        for (let j = 0; j < snake.body.length; j++) {
-            if (snake.body[j][1] == snake.head[1]) {
-                snake.body[j][0] = snake.body[j][0] - 1
-            } else if (snake.body[j][1] < snake.head[1]) {
-                snake.body[j][1] = snake.body[j][1] + 1
-            } else if (snake.body[j][1] > snake.head[1]) {
-                snake.body[j][1] = snake.body[j][1] - 1
+        gameState.snake.head[0] = gameState.snake.head[0] - 1
+        for (let j = 0; j < gameState.snake.body.length; j++) {
+            if (gameState.snake.body[j][1] == gameState.snake.head[1]) {
+                gameState.snake.body[j][0] = gameState.snake.body[j][0] - 1
+            } else if (gameState.snake.body[j][1] < gameState.snake.head[1]) {
+                gameState.snake.body[j][1] = gameState.snake.body[j][1] + 1
+            } else if (gameState.snake.body[j][1] > gameState.snake.head[1]) {
+                gameState.snake.body[j][1] = gameState.snake.body[j][1] - 1
             }
         }
         drawSnake()
     }
 
 
-    if (direction == '40') {
+    if (gameState.direction == '40') {
         deleteSnake()
-        snake.head[0] = snake.head[0] + 1
-        for (let j = 0; j < snake.body.length; j++) {
-            if (snake.body[j][1] == snake.head[1]) {
-                snake.body[j][0] = snake.body[j][0] + 1
-            } else if (snake.body[j][1] < snake.head[1]) {
-                snake.body[j][1] = snake.body[j][1] + 1
-            } else if (snake.body[j][1] > snake.head[1]) {
-                snake.body[j][1] = snake.body[j][1] - 1
+        gameState.snake.head[0] = gameState.snake.head[0] + 1
+        for (let j = 0; j < gameState.snake.body.length; j++) {
+            if (gameState.snake.body[j][1] == gameState.snake.head[1]) {
+                gameState.snake.body[j][0] = gameState.snake.body[j][0] + 1
+            } else if (gameState.snake.body[j][1] < gameState.snake.head[1]) {
+                gameState.snake.body[j][1] = gameState.snake.body[j][1] + 1
+            } else if (gameState.snake.body[j][1] > gameState.snake.head[1]) {
+                gameState.snake.body[j][1] = gameState.snake.body[j][1] - 1
             }
         }
         drawSnake()
     }
 
-    if (direction == '37') {
+    if (gameState.direction == '37') {
         deleteSnake()
-        snake.head[1] = snake.head[1] - 1
-        for (let j = 0; j < snake.body.length; j++) {
-            if (snake.body[j][0] == snake.head[0]) {
-                snake.body[j][1] = snake.body[j][1] - 1
-            } else if (snake.body[j][0] < snake.head[0]) {
-                snake.body[j][0] = snake.body[j][0] + 1
-            } else if (snake.body[j][0] > snake.head[0]) {
-                snake.body[j][0] = snake.body[j][0] - 1
+        gameState.snake.head[1] = gameState.snake.head[1] - 1
+        for (let j = 0; j < gameState.snake.body.length; j++) {
+            if (gameState.snake.body[j][0] == gameState.snake.head[0]) {
+                gameState.snake.body[j][1] = gameState.snake.body[j][1] - 1
+            } else if (gameState.snake.body[j][0] < gameState.snake.head[0]) {
+                gameState.snake.body[j][0] = gameState.snake.body[j][0] + 1
+            } else if (gameState.snake.body[j][0] > gameState.snake.head[0]) {
+                gameState.snake.body[j][0] = gameState.snake.body[j][0] - 1
             }
         }
         drawSnake()
     }
-    if (direction == '39') {
+    if (gameState.direction == '39') {
         deleteSnake();
-        console.log(snake.head);
-        snake.head[1] = snake.head[1] + 1
-        for (let j = 0; j < snake.body.length; j++) {
-            console.log(snake.head[0]);
-            console.log(snake.body[j][0]);
-            if (snake.body[j][0] == snake.head[0]) {
-                snake.body[j][1] = snake.body[j][1] + 1
-            } else if (snake.body[j][0] < snake.head[0]) {
-                snake.body[j][0] = snake.body[j][0] + 1
-            } else if (snake.body[j][0] > snake.head[0]) {
-                snake.body[j][0] = snake.body[j][0] - 1
+        console.log(gameState.snake.head);
+        gameState.snake.head[1] = gameState.snake.head[1] + 1
+        for (let j = 0; j < gameState.snake.body.length; j++) {
+            console.log(gameState.snake.head[0]);
+            console.log(gameState.snake.body[j][0]);
+            if (gameState.snake.body[j][0] == gameState.snake.head[0]) {
+                gameState.snake.body[j][1] = gameState.snake.body[j][1] + 1
+            } else if (gameState.snake.body[j][0] < gameState.snake.head[0]) {
+                gameState.snake.body[j][0] = gameState.snake.body[j][0] + 1
+            } else if (gameState.snake.body[j][0] > gameState.snake.head[0]) {
+                gameState.snake.body[j][0] = gameState.snake.body[j][0] - 1
             }
         }
         drawSnake()
     } eatingApple()
-    endGame()
 }
 
 function deleteSnake() {
-    for (let i = 0; i < snake.body.length; i++) {
-        table.children.item(snake.body[i][0]).children.item(snake.body[i][1]).removeAttribute('class');
+    for (let i = 0; i < gameState.snake.body.length; i++) {
+        table.children.item(gameState.snake.body[i][0]).children.item(gameState.snake.body[i][1]).removeAttribute('class');
     }
 }
 
 let started = true;
 
-function endGame() {
-    if (snake.head[0] == snake.body[snake.body.length - 1][0] && snake.head[1] == snake.body[snake.body.length - 1][1]) {
-        clearInterval(snakeInterval);
-        document.getElementById('best-score').innerHTML = 'Best: ' + score;
-        alert('Game over');
-    } else if (snake.head[0] == -1 || snake.head[1] == -1) {
-        clearInterval(snakeInterval);
-        document.getElementById('best-score').innerHTML = 'Best: ' + score;
-        alert('Game over');
-    } else if (snake.head[0] == 15 || snake.head[1] == 15) {
-        clearInterval(snakeInterval);
-        document.getElementById('best-score').innerHTML = 'Best: ' + score;
-        alert('Game over');
-    }
-}
+// function endGame() {
+//     if (gameState.snake.head[0] == gameState.snake.body[gameState.snake.body.length - 1][0] && gameState.snake.head[1] == gameState.snake.body[gameState.snake.body.length - 1][1]) {
+//         clearInterval(snakeInterval);
+//         document.getElementById('best-score').innerHTML = 'Best: ' + score;
+//         alert('Game over');
+//     } else if (gameState.snake.head[0] == -1 || gameState.snake.head[1] == -1) {
+//         clearInterval(snakeInterval);
+//         document.getElementById('best-score').innerHTML = 'Best: ' + score;
+//         alert('Game over');
+//     } else if (gameState.snake.head[0] == 15 || gameState.snake.head[1] == 15) {
+//         clearInterval(snakeInterval);
+//         document.getElementById('best-score').innerHTML = 'Best: ' + score;
+//         alert('Game over');
+//     }
+// }
 
-let snakeInterval = setInterval(moveSnake, 1000);
+
+// write functions
 
 
 
